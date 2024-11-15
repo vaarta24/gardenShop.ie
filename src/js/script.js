37e9c33daf8181a820e1b38291c3aab71d7402d7
vaarta24 <vanessa.tamboril.ara@gmail.com>
2024-11-15 21:21:56 +0000

    Add files via upload
    versao final ?

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    /*funcao do botao  */
+    $('#mobile-btn').on('click', function (){
+        $('#mobile-menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+    
